b027f739199224bc09d20a8b0f3bdeb201d9f01c
leonelacs <me@leonelacs.com>
2018-12-03 10:22:35 +0800

    Initial commit

---
+++ CreatRand/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace CreatRand
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.nudLower = new System.Windows.Forms.NumericUpDown();
+            this.nudUpper = new System.Windows.Forms.NumericUpDown();
+            this.nudAmount = new System.Windows.Forms.NumericUpDown();
+            this.lblRange = new System.Windows.Forms.Label();
+            this.lblLower = new System.Windows.Forms.Label();
+            this.lblUpper = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.lblSign = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudLower)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudUpper)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAmount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudLower
+            // 
+            this.nudLower.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.nudLower.Location = new System.Drawing.Point(76, 43);
+            this.nudLower.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nudLower.Name = "nudLower";
+            this.nudLower.Size = new System.Drawing.Size(63, 26);
+            this.nudLower.TabIndex = 0;
+            // 
+            // nudUpper
+            // 
+            this.nudUpper.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.nudUpper.Location = new System.Drawing.Point(77, 75);
+            this.nudUpper.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.nudUpper.Name = "nudUpper";
+            this.nudUpper.Size = new System.Drawing.Size(63, 26);
+            this.nudUpper.TabIndex = 1;
+            this.nudUpper.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // nudAmount
+            // 
+            this.nudAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.nudAmount.Location = new System.Drawing.Point(22, 155);
+            this.nudAmount.Maximum = new decimal(new int[] {
+            99999999,
+            0,
+            0,
+            0});
+            this.nudAmount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudAmount.Name = "nudAmount";
+            this.nudAmount.Size = new System.Drawing.Size(117, 26);
+            this.nudAmount.TabIndex = 2;
+            this.nudAmount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // lblRange
+            // 
+            this.lblRange.AutoSize = true;
+            this.lblRange.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblRange.Location = new System.Drawing.Point(17, 7);
+            this.lblRange.Name = "lblRange";
+            this.lblRange.Size = new System.Drawing.Size(84, 29);
+            this.lblRange.TabIndex = 3;
+            this.lblRange.Text = "Range";
+            // 
+            // lblLower
+            // 
+            this.lblLower.AutoSize = true;
+            this.lblLower.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblLower.Location = new System.Drawing.Point(18, 45);
+            this.lblLower.Name = "lblLower";
+            this.lblLower.Size = new System.Drawing.Size(52, 20);
+            this.lblLower.TabIndex = 4;
+            this.lblLower.Text = "Lower";
+            // 
+            // lblUpper
+            // 
+            this.lblUpper.AutoSize = true;
+            this.lblUpper.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblUpper.Location = new System.Drawing.Point(18, 77);
+            this.lblUpper.Name = "lblUpper";
+            this.lblUpper.Size = new System.Drawing.Size(53, 20);
+            this.lblUpper.TabIndex = 5;
+            this.lblUpper.Text = "Upper";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(17, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Amount";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnCreate.Location = new System.Drawing.Point(22, 201);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(243, 46);
+            this.btnCreate.TabIndex = 7;
+            this.btnCreate.Text = "GENERATE";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // lblSign
+            // 
+            this.lblSign.AutoSize = true;
+            this.lblSign.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblSign.Location = new System.Drawing.Point(159, 56);
+            this.lblSign.Name = "lblSign";
+            this.lblSign.Size = new System.Drawing.Size(0, 24);
+            this.lblSign.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Meiryo", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(178, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "(C) 2018 みずは";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(133, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 59);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "If the program has no respond, please wait until it is completed.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblSign);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblUpper);
+            this.Controls.Add(this.lblLower);
+            this.Controls.Add(this.lblRange);
+            this.Controls.Add(this.nudAmount);
+            this.Controls.Add(this.nudUpper);
+            this.Controls.Add(this.nudLower);
+            this.Controls.Add(this.label3);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(300, 300);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Form1";
+            this.Text = "RandGen";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudLower)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudUpper)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAmount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudLower;
+        private System.Windows.Forms.NumericUpDown nudUpper;
+        private System.Windows.Forms.NumericUpDown nudAmount;
+        private System.Windows.Forms.Label lblRange;
+        private System.Windows.Forms.Label lblLower;
+        private System.Windows.Forms.Label lblUpper;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Label lblSign;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CreatRand/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreatRand
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static int GetRandomSeed()
+        {
+            byte[] bytes = new byte[4];
+            System.Security.Cryptography.RNGCryptoServiceProvider rng = new System.Security.Cryptography.RNGCryptoServiceProvider();
+            rng.GetBytes(bytes);
+            return BitConverter.ToInt32(bytes, 0);
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            if (nudLower.Value >= nudUpper.Value)
+            {
+                lblSign.Text = "Range error!";
+                lblSign.ForeColor = Color.Red;
+                return;
+            }
+            lblSign.Text = "Generating...";
+            lblSign.ForeColor = Color.DeepSkyBlue;
+            List<int> Num = new List<int>();
+            Random rd;
+            for(int i = 0; i < nudAmount.Value; i++)
+            {
+                rd = new Random(GetRandomSeed());
+                Num.Add(rd.Next((int)nudLower.Value, (int)nudUpper.Value + 1));
+            }
+            using (StreamWriter sw = new StreamWriter("random.txt"))
+            {
+                //sw.WriteLine(nudAmount.Value);
+                foreach (int num in Num)
+                {
+                    sw.WriteLine(num);
+                }
+            }
+            lblSign.Text = "Data saved";
+            lblSign.ForeColor = Color.DarkGreen;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
